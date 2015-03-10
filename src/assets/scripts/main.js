5031b3bae579a8329f5c92e6fa420212c31ed0b6
Matthew Slavin <mslavin@sapient.com>
2015-03-10 18:57:04 -0400

    test

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,25 +1,27 @@
-export class Employee {
-	constructor(name){
-		this._name = name;
-	}
+// export class Employee {
+// 	constructor(name){
+// 		this._name = name;
+// 	}
 
-	get name(){
-		return this._name;
-	}
+// 	get name(){
+// 		return this._name;
+// 	}
 
-	doWork {
-		return '${this._name} is working';
-	}
-}
-
-// function testModule(){
-// 	var foo = function(){
-// 		console.log("Return foo result");
-// 	};
-
-// 	var bar = function(){
-// 		console.log("Return bar result");
-// 	};
-
-// 	return bar;
+// 	doWork {
+// 		return '${this._name} is working';
+// 	}
 // }
+
+function testModule(){
+
+
+	this.foo = function(){
+		console.log("Return foo result");
+	};
+
+	this.bar = function(){
+		console.log("Return bar result");
+	};
+
+	return this;
+}
