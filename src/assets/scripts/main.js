8cca1eb525710386bd385fd5d73bd1bb8daabc0e
Matthew Slavin <mslavin@sapient.com>
2015-03-10 18:58:41 -0400

    Test

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -13,15 +13,13 @@
 // }
 
 function testModule(){
-
-
-	this.foo = function(){
+	var foo = function(){
 		console.log("Return foo result");
 	};
 
-	this.bar = function(){
+	var bar = function(){
 		console.log("Return bar result");
 	};
 
-	return this;
+	return bar;
 }
